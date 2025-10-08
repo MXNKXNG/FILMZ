027a0b8cb15470d344aa13c238faf32d2868ea85
min <oldscratch.co@gmail.com>
2025-10-09 00:41:26 +0900

    feat: set up Mypage layout

--- src/components/ProfileLayout.jsx
+++ src/components/ProfileLayout.jsx
@@ -1,45 +1,62 @@
-import { NavLink, Outlet } from "react-router";
-import profile from "../assets/profile.png";
+import { useState } from "react";
+import { NavLink, Outlet, useLocation } from "react-router";
 import { useSupabase } from "../context/AuthProvider";
 
 export const ProfileLayout = () => {
   const { session } = useSupabase();
+  const [showNav, setShowNav] = useState(false);
+  const { pathname } = useLocation();
+  const navList = [
+    { label: "프로필 홈", path: `/mypage/${session?.user?.id}`, end: true },
+    { label: "내 정보", path: `/mypage/info/${session?.user?.id}` },
+    { label: "내가 찜한 리스트", path: `/mypage/list/${session?.user?.id}` },
+  ];
+
+  const activeIndex =
+    navList.findIndex((el) => el.path === pathname) >= 0
+      ? navList.findIndex((el) => el.path === pathname)
+      : navList.findIndex((el) => pathname.startsWith(el.path));
+  const activeList = navList[activeIndex] ?? navList[0];
+  const restList = navList.filter((_, i) => i !== activeIndex);
 
   return (
-    <div className="h-dvh w-full bg-radial-[at_15%_10%] from-[#1f202d] to-[#060c18] to-30% whitespace-nowrap font-normal text-white text-base min-[1024px]:text-lg min-[2048px]:text-2xl">
-      <section className="fixed flex flex-col gap-6 py-10 w-60 border-gray-500 border rounded-2xl bg-[#060c18]">
-        <article className="flex flex-col justify-center items-center pb-10 gap-2 border-gray-500 border-b">
-          <img
-            className="bg-white rounded-full"
-            src={profile}
-            width={120}
-            alt="profile Image"
-          />
-          <p>{session?.user?.email}</p>
-        </article>
-        <nav className="flex flex-col gap-4 px-4">
+    <div className="flex flex-col h-dvh w-full bg-radial-[at_15%_10%] from-[#1f202d] to-[#060c18] to-30% whitespace-nowrap font-normal text-white text-base min-[1024px]:text-lg min-[2048px]:text-2xl">
+      <section className="flex flex-col mt-6 w-full gap-2 max-[513px]:text-sm text-base min-[2048px]:text-xl">
+        <div className="flex justify-between items-center px-4">
           <NavLink
-            to={`/mypage/${session?.user?.id}`}
-            end
-            className=" cursor-pointer active:scale-90 aria-[current=page]:text-rose-500 aria-[current=page]:font-semibold"
+            className="text-[#a52d43]"
+            to={activeList.path}
+            end={Boolean(activeList.end)}
           >
-            프로필
+            {activeList.label}
           </NavLink>
-          <NavLink
-            to={`/mypage/info/${session?.user?.id}`}
-            className=" cursor-pointer active:scale-90 aria-[current=page]:text-rose-500 aria-[current=page]:font-semibold"
+
+          <button
+            className="px-4 pb-2 cursor-pointer flex justify-center items-center"
+            onClick={() => setShowNav((prev) => !prev)}
           >
-            내 정보
-          </NavLink>
-          <NavLink
-            to={`/mypage/list/${session?.user?.id}`}
-            className=" cursor-pointer active:scale-90 aria-[current=page]:text-rose-500 aria-[current=page]:font-semibold"
-          >
-            내가 찜한 리스트
-          </NavLink>
+            ⌄
+          </button>
+        </div>
+
+        <nav
+          className={`flex flex-col items-start w-4/5 ${
+            showNav ? "opacity-100 translate-0" : "opacity-0 -translate-y-2"
+          } items-center gap-2 transition-all duration-300`}
+        >
+          {restList.map((el) => (
+            <NavLink
+              key={el.path}
+              end={Boolean(el.end)}
+              to={el.path}
+              className="px-4 cursor-pointer opacity-60 transition-all duration-300 active:scale-90"
+            >
+              {el.label}
+            </NavLink>
+          ))}
         </nav>
       </section>
-      <div className="ml-60 py-10 px-4">
+      <div className="p-6">
         <Outlet />
       </div>
     </div>
--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,7 +1,27 @@
+import profile from "../assets/profile.png";
+import { useSupabase } from "../context/AuthProvider";
+import { useProfile } from "../features/profiles/useProfile";
+
 export const MyPage = () => {
+  const { session } = useSupabase();
+  const { data } = useProfile(session?.user?.id);
+
   return (
     <section className="flex flex-col w-full h-full">
-      <div>마이 페이지 초기 화면</div>
+      <article className="flex flex-col justify-center items-center">
+        <img
+          className="bg-white rounded-full"
+          src={profile}
+          width={120}
+          alt="profile Image"
+        />
+        {data && (
+          <div className="flex flex-col justify-center items-center gap-1 p-1">
+            <p title="nickname">{data?.nickname}</p>
+            <p title="email">{data?.email}</p>
+          </div>
+        )}
+      </article>
     </section>
   );
 };
